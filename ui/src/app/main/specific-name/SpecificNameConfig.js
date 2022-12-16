0b7b15d5186756607a8c179adcf2742f0460542c
Dominik Grzegolec <grzegolecdominik@gmail.com>
2022-12-16 16:08:43 +0100

    created new sidebar link

---
+++ ui/src/app/main/specific-name/SpecificNameConfig.js
@@ -0,0 +1,51 @@
+import i18next from 'i18next';
+
+import en from './i18n/en';
+import tr from './i18n/tr';
+import ar from './i18n/ar';
+// import Example from './Example';
+
+i18next.addResourceBundle('en', 'examplePage', en);
+i18next.addResourceBundle('tr', 'examplePage', tr);
+i18next.addResourceBundle('ar', 'examplePage', ar);
+
+// const ExampleConfig = {
+//   settings: {
+//     layout: {
+//       config: {},
+//     },
+//   },
+//   routes: [
+//     {
+//       path: 'example',
+//       element: <Example />,
+//     },
+//   ],
+// };
+
+// export default ExampleConfig;
+
+/**
+ * Lazy load Example
+ */
+
+import React from 'react';
+
+const SpecificName= React.lazy(() => import('./SpecificName'));
+
+const ExampleConfig = {
+  settings: {
+    layout: {
+      config: {},
+    },
+  },
+  routes: [
+    {
+      path: 'specific-name',
+      element: <SpecificName/>,
+    },
+  ],
+};
+
+export default ExampleConfig;
+
