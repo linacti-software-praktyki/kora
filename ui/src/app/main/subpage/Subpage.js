5e04c3f9afdf0d6e3bfb8339825b8434ed5beee8
Jakubmus <musial.jak@gmail.com>
2022-12-20 08:13:09 +0100

    Generic SubPage view created

--- ui/src/app/main/subpage/Subpage.js
+++ ui/src/app/main/subpage/Subpage.js
@@ -18,8 +18,23 @@ function SubPage(props) {
     return (
       <Root
         header={
-          <div className="p-24">
-            <h4>MY SUBPAGE</h4>
+          <div>
+            <p><h2>MY<span>SUBPAGE</span></h2></p>
+          </div>
+        }
+        content={
+          <div>
+            <div>
+              <img src='/assets/images/demo-content/morain-lake.jpg'></img>
+              <br></br>
+              <h1><b>Early Sunrise</b></h1>
+              <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. </p>
+              <br></br>
+              <a href='post'>Read more!</a>
+            </div>
+            <div>
+              <p>This SubPage have been created by JakubMus</p>
+            </div>
           </div>
         }
       />
