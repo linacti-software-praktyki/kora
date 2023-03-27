ffb5181ae1eedfeefc81ca86184900dd42f349ff
vilible <stefanskipiotr.8@proton.me>
2023-03-27 21:57:41 +0200

    Created new finances app

--- app/api.py
+++ app/api.py
@@ -1,7 +1,9 @@
 from app.blog.endpoints import router as blog_router
+from app.finances.endpoints import router as finance_router
 # from app.contact.endpoints import router as contact_router
 from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(blog_router, prefix="/blog", tags=["Blog"])
+router.include_router(finance_router, prefix="/finances", tags=["Finances"])
 # router.include_router(contact_router, prefix="/contact", tags=["Contact"])
